73c3f2a626db54afe85d85a1dcc4bcd1c57633dc
Dylan <dylanharris23@gmail.com>
2019-06-05 17:44:37 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+    attr_accessor :name
+    
+    def initialize(name)
+        @name = name
+    end
+
+    def name 
+        @name
+    end
+
+end
+
+beyonce = Person.new("Beyonce")
