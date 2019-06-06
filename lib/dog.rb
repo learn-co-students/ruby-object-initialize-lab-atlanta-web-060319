77abe38f59e213fb1c5dec8aa7257e48391ac62c
iheartobi <iobidah9@gmail.com>
2019-06-05 22:00:57 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,16 +3,16 @@ class Dog
         @name = name
         @breed = breed
     end
-    def name=(name)
-        @name = name
-    end
-    def name
-        @name
-    end
-    def breed=(breed)
-        @breed = breed
-    end
-    def breed
-        @breed
-    end
+    # def name=(name)
+    #     @name = name
+    # end
+    # def name
+    #     @name
+    # end
+    # def breed=(breed)
+    #     @breed = breed
+    # end
+    # def breed
+    #     @breed
+    # end
 end
